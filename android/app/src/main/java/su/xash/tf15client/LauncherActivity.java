4325d1b0bc0a868f55dfa200695be3ec03bae855
Velaron <mars873@gmail.com>
2022-01-31 17:18:18 +0200

    add donate button :3

--- android/app/src/main/java/su/xash/tf15client/LauncherActivity.java
+++ android/app/src/main/java/su/xash/tf15client/LauncherActivity.java
@@ -1,7 +1,7 @@
 package su.xash.tf15client;
 
+import android.annotation.SuppressLint;
 import android.content.ComponentName;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
@@ -13,8 +13,6 @@ import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
@@ -23,11 +21,11 @@ import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputEditText;
 
 import org.json.JSONException;
-import org.json.JSONObject;
 
 public class LauncherActivity extends AppCompatActivity {
 	private static final int XASH_MIN_VERSION = 1710;
 
+	@SuppressLint("SetTextI18n")
 	@Override
 	public void onCreate(Bundle savedInstanceBundle) {
 		super.onCreate(savedInstanceBundle);
@@ -35,20 +33,22 @@ public class LauncherActivity extends AppCompatActivity {
 
 		ExtractAssets.extractPAK(this, false);
 
-		TextInputEditText launchParameters = (TextInputEditText) findViewById(R.id.launchParameters);
+		TextInputEditText launchParameters = findViewById(R.id.launchParameters);
 		launchParameters.setText("-log -dev 2");
 
-		ExtendedFloatingActionButton launchButton = (ExtendedFloatingActionButton) findViewById(R.id.launchButton);
-		launchButton.setOnClickListener((view) -> {
-			startActivity(new Intent().setComponent(new ComponentName("su.xash.engine", "su.xash.engine.XashActivity"))
-					.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-					.putExtra("pakfile", getExternalFilesDir(null).getAbsolutePath() + "/extras.pak")
-					.putExtra("gamedir", "tfc")
-					.putExtra("argv", launchParameters.getText())
-					.putExtra("gamelibdir", getApplicationInfo().nativeLibraryDir));
-		});
+		ExtendedFloatingActionButton launchButton = findViewById(R.id.launchButton);
+		launchButton.setOnClickListener((view) -> startActivity(new Intent().setComponent(new ComponentName("su.xash.engine", "su.xash.engine.XashActivity"))
+				.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+				.putExtra("pakfile", getExternalFilesDir(null).getAbsolutePath() + "/extras.pak")
+				.putExtra("gamedir", "tfc")
+				.putExtra("argv", launchParameters.getText())
+				.putExtra("gamelibdir", getApplicationInfo().nativeLibraryDir)));
 
 		launchButton.setEnabled(false);
+
+		ExtendedFloatingActionButton donateButton = findViewById(R.id.donateButton);
+		donateButton.setOnClickListener(v -> startActivity(new Intent(Intent.ACTION_VIEW)
+				.setData(Uri.parse("https://www.buymeacoffee.com/velaron"))));
 
 		checkForEngine();
 	}
@@ -71,39 +71,33 @@ public class LauncherActivity extends AppCompatActivity {
 
 	private void checkForUpdates() {
 		String url = "https://api.github.com/repos/Velaron/tf15-client/commits/master";
-		CoordinatorLayout contextView = (CoordinatorLayout) findViewById(R.id.coordinatorLayout);
-		ExtendedFloatingActionButton launchButton = (ExtendedFloatingActionButton) findViewById(R.id.launchButton);
+		CoordinatorLayout contextView = findViewById(R.id.coordinatorLayout);
+		ExtendedFloatingActionButton launchButton = findViewById(R.id.launchButton);
 
 		Snackbar updateNotification = Snackbar.make(contextView, R.string.checking_for_updates, Snackbar.LENGTH_INDEFINITE).setAnchorView(launchButton);
 		updateNotification.show();
 
-		JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-			@Override
-			public void onResponse(JSONObject response) {
-				try {
-					String sha = response.getString("sha").substring(0, 7);
-					String version_sha = getPackageManager().getPackageInfo(getPackageName(), 0).versionName.split("-")[1];
+		JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, response -> {
+			try {
+				String sha = response.getString("sha").substring(0, 7);
+				String version_sha = getPackageManager().getPackageInfo(getPackageName(), 0).versionName.split("-")[1];
 
-					if (version_sha.equals(sha)) {
-						updateNotification.dismiss();
-						launchButton.setEnabled(true);
-					} else {
-						updateNotification.dismiss();
-						openDialog("https://github.com/Velaron/tf15-client/releases/tag/continuous", getString(R.string.update_required),
-								getString(R.string.update_available, "TF15Client"));
-					}
-				} catch (JSONException e) {
-					e.printStackTrace();
-				} catch (PackageManager.NameNotFoundException e) {
-					e.printStackTrace();
+				if (version_sha.equals(sha)) {
+					updateNotification.dismiss();
+					launchButton.setEnabled(true);
+				} else {
+					updateNotification.dismiss();
+					openDialog("https://github.com/Velaron/tf15-client/releases/tag/continuous", getString(R.string.update_required),
+							getString(R.string.update_available, "TF15Client"));
 				}
+			} catch (JSONException e) {
+				e.printStackTrace();
+			} catch (PackageManager.NameNotFoundException e) {
+				e.printStackTrace();
 			}
-		}, new Response.ErrorListener() {
-			@Override
-			public void onErrorResponse(VolleyError error) {
-				updateNotification.dismiss();
-				launchButton.setEnabled(true);
-			}
+		}, error -> {
+			updateNotification.dismiss();
+			launchButton.setEnabled(true);
 		});
 
 		RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
@@ -115,17 +109,7 @@ public class LauncherActivity extends AppCompatActivity {
 				.setTitle(title)
 				.setMessage(description)
 				.setCancelable(false)
-				.setNegativeButton(R.string.exit, new DialogInterface.OnClickListener() {
-					@Override
-					public void onClick(DialogInterface dialog, int which) {
-						finish();
-					}
-				})
-				.setPositiveButton(R.string.update, new DialogInterface.OnClickListener() {
-					@Override
-					public void onClick(DialogInterface dialog, int which) {
-						startActivity(new Intent(Intent.ACTION_VIEW).setData(Uri.parse(url)));
-					}
-				}).show();
+				.setNegativeButton(R.string.exit, (dialog, which) -> finish())
+				.setPositiveButton(R.string.update, (dialog, which) -> startActivity(new Intent(Intent.ACTION_VIEW).setData(Uri.parse(url)))).show();
 	}
 }
