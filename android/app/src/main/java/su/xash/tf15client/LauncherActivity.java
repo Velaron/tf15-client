cc395d96fe089f0c53c69fcac6b946c6473b0353
Velaron <mars873@gmail.com>
2022-03-29 19:16:42 +0300

    much better check, should be ok now

--- android/app/src/main/java/su/xash/tf15client/LauncherActivity.java
+++ android/app/src/main/java/su/xash/tf15client/LauncherActivity.java
@@ -5,9 +5,11 @@ import android.content.ComponentName;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.content.res.Resources;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
@@ -24,11 +26,16 @@ import com.google.android.material.textfield.TextInputEditText;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class LauncherActivity extends AppCompatActivity {
-	private static final int XASH_MIN_VERSION = 1710;
 	private static final String COMMITS_URL = "https://api.github.com/repos/Velaron/tf15-client/commits/master";
-	private static final String RUNS_URL = "https://api.github.com/repos/Velaron/tf15-client/actions/runs";
+	private static final String RUNS_URL = "https://api.github.com/repos/Velaron/tf15-client/actions/runs?branch=master";
 	private static final String APK_URL = "https://github.com/Velaron/tf15-client/releases/download/continuous/tf15-client.apk";
+
+	private static final String ENGINE_COMMITS_URL = "https://api.github.com/repos/FWGS/xash3d-fwgs/commits/master";
+	private static final String ENGINE_RUNS_URL = "https://api.github.com/repos/FWGS/xash3d-fwgs/actions/runs?branch=master";
 
 	@SuppressLint("SetTextI18n")
 	@Override
@@ -81,16 +88,7 @@ public class LauncherActivity extends AppCompatActivity {
 				return;
 			}
 
-			if (info.versionCode < XASH_MIN_VERSION) {
-				new MaterialAlertDialogBuilder(LauncherActivity.this)
-						.setTitle(R.string.update_required)
-						.setMessage(getString(R.string.update_available, "Xash3D FWGS"))
-						.setCancelable(true)
-						.setNegativeButton(R.string.later, null)
-						.setPositiveButton(R.string.update, (dialog, which) -> startActivity(new Intent(Intent.ACTION_VIEW).setData(Uri.parse(getEngineDownloadUrl())))).show();
-				return;
-			}
-
+			checkForEngineUpdates();
 			checkForUpdates();
 		} catch (PackageManager.NameNotFoundException e) {
 			new MaterialAlertDialogBuilder(LauncherActivity.this)
@@ -100,6 +98,64 @@ public class LauncherActivity extends AppCompatActivity {
 					.setNegativeButton(R.string.later, null)
 					.setPositiveButton(R.string.update, (dialog, which) -> startActivity(new Intent(Intent.ACTION_VIEW).setData(Uri.parse(getEngineDownloadUrl())))).show();
 		}
+	}
+
+	private void checkForEngineUpdates() {
+		CoordinatorLayout contextView = findViewById(R.id.coordinatorLayout);
+		ExtendedFloatingActionButton launchButton = findViewById(R.id.launchButton);
+
+		Snackbar updateNotification = Snackbar.make(contextView, R.string.checking_for_updates, Snackbar.LENGTH_INDEFINITE).setAnchorView(launchButton);
+		updateNotification.show();
+
+		RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+
+		JsonObjectRequest commitsRequest = new JsonObjectRequest(Request.Method.GET, ENGINE_COMMITS_URL, null, commitsResponse -> {
+			try {
+				String sha = commitsResponse.getString("sha").substring(0, 7);
+
+				JsonObjectRequest runsRequest = new JsonObjectRequest(Request.Method.GET, ENGINE_RUNS_URL, null, runsResponse -> {
+					try {
+						JSONArray workflowRuns = runsResponse.getJSONArray("workflow_runs");
+						String status = workflowRuns.getJSONObject(0).getString("status");
+						String conclusion = workflowRuns.getJSONObject(0).getString("conclusion");
+
+						Resources res = getPackageManager().getResourcesForApplication("su.xash.engine");
+						int resId = res.getIdentifier("git_revisions", "string", "su.xash.engine");
+						String gitRevisions = res.getString(resId);
+
+						// developer build
+						if (!gitRevisions.equals("No git version information")) {
+							Pattern p = Pattern.compile("(?<=\\bxash3d-fwgs:\\s)(\\w+)");
+							Matcher m = p.matcher(gitRevisions);
+
+							if (m.find()) {
+								String engineCommitHash = m.group();
+
+								if (engineCommitHash.equals(sha) && status.equals("completed") && conclusion.equals("success")) {
+									new MaterialAlertDialogBuilder(LauncherActivity.this)
+											.setTitle(R.string.update_required)
+											.setMessage(getString(R.string.update_available, "Xash3D FWGS"))
+											.setCancelable(true)
+											.setNegativeButton(R.string.later, null)
+											.setPositiveButton(R.string.update, (dialog, which) -> startActivity(new Intent(Intent.ACTION_VIEW).setData(Uri.parse(getEngineDownloadUrl())))).show();
+								}
+							}
+						}
+
+						updateNotification.dismiss();
+					} catch (JSONException | PackageManager.NameNotFoundException e) {
+						e.printStackTrace();
+					}
+				}, error -> updateNotification.dismiss());
+
+				requestQueue.add(runsRequest);
+
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+		}, error -> updateNotification.dismiss());
+
+		requestQueue.add(commitsRequest);
 	}
 
 	private void checkForUpdates() {
