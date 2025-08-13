8c412fbcb7d84f5e8da1091f8719c7021fa51237
Bohdan Shulyar <mars873@gmail.com>
2025-08-13 16:30:23 +0300

    android: fix formatting

--- android/app/src/main/java/su/xash/tf15client/MainActivity.java
+++ android/app/src/main/java/su/xash/tf15client/MainActivity.java
@@ -9,30 +9,33 @@ import android.os.Bundle;
 
 
 public class MainActivity extends Activity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-        String pkg = "su.xash.engine.test";
+		String pkg = "su.xash.engine.test";
 
-        try {
-            getPackageManager().getPackageInfo(pkg, 0);
-        } catch (PackageManager.NameNotFoundException e) {
-            try {
-                pkg = "su.xash.engine";
-                getPackageManager().getPackageInfo(pkg, 0);
-            } catch (PackageManager.NameNotFoundException ex) {
-                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/FWGS/xash3d-fwgs/releases/tag/continuous")).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK));
-                finish();
-                return;
-            }
-        }
+		try {
+			getPackageManager().getPackageInfo(pkg, 0);
+		} catch (PackageManager.NameNotFoundException e) {
+			try {
+				pkg = "su.xash.engine";
+				getPackageManager().getPackageInfo(pkg, 0);
+			} catch (PackageManager.NameNotFoundException ex) {
+				startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/FWGS/xash3d-fwgs/releases/tag/continuous")).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK));
+				finish();
+				return;
+			}
+		}
 
-        startActivity(new Intent().setComponent(new ComponentName(pkg, "su.xash.engine.XashActivity"))
-                .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK)
-                .putExtra("gamedir", "tfc").putExtra("gamelibdir", getApplicationInfo().nativeLibraryDir)
-                .putExtra("argv", "-dev 2 -log")
-                .putExtra("package", getPackageName()));
-        finish();
-    }
+		startActivity(new Intent()
+				.setComponent(new ComponentName(pkg, "su.xash.engine.XashActivity"))
+				.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK)
+				.putExtra("gamedir", "tfc")
+				.putExtra("gamelibdir", getApplicationInfo().nativeLibraryDir)
+				.putExtra("argv", "-dev 2 -log")
+				.putExtra("package", getPackageName()));
+
+		finish();
+	}
 }
